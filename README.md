3b5bd63538ad7588b260c273d23bb9584b931c4b
prayge <sgjfxvs@gmail.com>
2022-07-31 10:29:15 +0100

    Update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 Abstract Image Generator
 
+Datasets used:
+
+Around 11000 images in total, 1000 coming from the scraper created and the rest coming from the datasets provided below.
+
+
+https://www.kaggle.com/datasets/bryanb/abstract-art-gallery - 2000+
+https://www.kaggle.com/datasets/greg115/abstract-art 8000+ 
--- dicttojson.py
+++ dicttojson.py
@@ -2,17 +2,29 @@ from os import listdir
 from os.path import isfile, join
 import os
 import json
+import itertools
+root = "https://swipeart.s3.eu-west-2.amazonaws.com/pics/"
 
-onlyfiles = [f for f in listdir("D://gan//ABS")
-             if isfile(join("D://gan//ABS", f))]
+onlyfiles = [f for f in listdir("D:/gan/pics")
+             if isfile(join("D:/gan/pics", f))]
 print(len(onlyfiles))
 
 
-d = {}
+d = {"categories":
+     {"all": []}
+     }
 for i in range(0, len(onlyfiles)):
     dd = {}
-    dd["src"] = onlyfiles[i]
-    d[i] = dd
+    user = {}
+    dd["id"] = i
+    dd["src"] = f"{root}{onlyfiles[i]}"
+    dd["liked"] = False
+    user["src"] = f"{root}pfp.jpg"
+    user["name"] = "Keito"
+    user["job"] = "Graphic Designer"
+    dd["user"] = user
 
-with open("sample.json", "w") as outfile:
+    d["categories"]["all"].append(dd)
+
+with open("Test.json", "w") as outfile:
     json.dump(d, outfile, indent=4, sort_keys=True)
